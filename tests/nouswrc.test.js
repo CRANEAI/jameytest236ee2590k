f56004e4548a550525ffca5569c9a76fbba1f4c6
Elly <elly@mg.crane.ai>
2018-11-02 15:26:39 -0400

    added tests/nouswrc refactoring

---
+++ tests/nouswrc.test.js
@@ -0,0 +1,21 @@
+/* 
+ * Contact: team@fusioncode.ai
+ *
+ *
+ * Project  - nouswrc UNIT TESTS
+ * Application Client
+ * Jamey Test
+*/
+
+/* the nouswrc view has a arrow icon located at the mid-top right of the view which on click will trigger the arrow() action */
+it("the nouswrc view has a arrow icon located at the mid-top right of the view which on click will trigger the arrow() action", () => {
+  /* ToDo developer needs to complete unit test 
+               the nouswrc view has a arrow icon located at the mid-top right of the view which on click will trigger the arrow() action
+               uncomment below and update the code to test
+                //instance.action(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+               */
+})
