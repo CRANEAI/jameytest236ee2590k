ba9782c74bbfc7d819d1d113944bc2c21d9d8292
Elly <elly@mg.crane.ai>
2018-11-02 15:24:26 -0400

    added tests/order_status refactoring

---
+++ tests/order_status.test.js
@@ -0,0 +1,21 @@
+/* 
+ * Contact: team@fusioncode.ai
+ *
+ *
+ * Project  - order status UNIT TESTS
+ * Application Client
+ * jamey test2
+*/
+
+/* order status view has Table Row located at the mid-bottom across the full width */
+it("order status view has Table Row located at the mid-bottom across the full width", () => {
+  /* ToDo developer needs to complete unit test 
+               order status view has Table Row located at the mid-bottom across the full width
+               uncomment below and update the code to test
+                //instance.action(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+               */
+})
